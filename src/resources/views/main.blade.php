6818c8c707b6e551c757d26fc80e430398c66f1a
eug3nius <eugene.boz@gmail.com>
2020-02-23 20:04:52 +0100

    Display authors books table #1
    Signed-off-by: Eu <eugene.boz@gmail.com>

--- src/resources/views/main.blade.php
+++ src/resources/views/main.blade.php
@@ -12,10 +12,44 @@
     </div>
     <br/>
 
-    <p>
 
+    <form action="/" method="POST"  class="">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Author's:</span>
+            </div>
+            <input type="text" name="firstName" class="form-control" placeholder="First Name"/>
+            <input type="text" name="lastName" class="form-control" placeholder="Last Name"/>
+            <input type="text" name="age" class="form-control" placeholder="Age"/>
+            <input type="text" name="address" class="form-control" placeholder="Address"/>
+        </div>
+
+        <!-- Books' validation errors -->
+        @if(!empty($errors->first()))
+            <div class="alert alert-danger" role="alert">
+                <li>{{ $errors->first('address') }}</li>
+                <li>{{ $errors->first('releaseDate') }}</li>
+            </div>
+        @endif
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Book's:</span>
+            </div>
+            <input type="text" name="bookTitle" class="form-control" placeholder="Title"/>
+            <input type="text" name="releaseDate"  class="form-control" placeholder="Release Date"/>
+        </div>
+
+        <button type="submit" class="btn btn-outline-success">Add Book</button>
+
+        {{--Validation for cross-origin--}}
+        @csrf
+    </form>
+
+    <br/>
+
+    <p>
         <button class="btn btn-outline-secondary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-            Add New Book
+            Show Collection
         </button>
         <button class="btn btn-info" type="button">
             Refresh
@@ -24,52 +58,16 @@
     <div class="collapse" id="collapseExample">
         <div class="card card-body">
 
-            <form action="/" method="POST"  class="">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text">Author's:</span>
-                    </div>
-                    <input type="text" name="firstName" class="form-control" placeholder="First Name"/>
-                    <input type="text" name="lastName" class="form-control" placeholder="Last Name"/>
-                    <input type="text" name="age" class="form-control" placeholder="Age"/>
-                    <input type="text" name="address" class="form-control" placeholder="Address"/>
-                </div>
-
-                <!-- Books' validation errors -->
-                @if(!empty($errors->first()))
-                    <div class="alert alert-danger" role="alert">
-                        <li>{{ $errors->first('address') }}</li>
-                        <li>{{ $errors->first('releaseDate') }}</li>
-                    </div>
-                @endif
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text">Book's:</span>
-                    </div>
-                    <input type="text" name="bookTitle" class="form-control" placeholder="Title"/>
-                    <input type="text" name="releaseDate"  class="form-control" placeholder="Release Date"/>
-                </div>
-
-                <button type="submit" class="btn btn-outline-success">Add Book</button>
-
-                {{--Validation for cross-origin--}}
-                @csrf
-            </form>
-
-        </div>
-    </div>
-
-    <div class="container">
-        <h2>My collection</h2>
-        <table class="table table-dark table-striped">
-            <thead>
+            <h2>My collection</h2>
+            <table class="table table-dark table-striped">
+                <thead>
                 <tr>
                     <th>Author</th>
                     <th>Book</th>
                     <th>Release Date</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @foreach($authors as $author)
                     <tr>
                         <td>{{ $author->name }}</td>
@@ -77,8 +75,10 @@
                         <td>john@example.com</td>
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+
+        </div>
     </div>
 
 </div>
