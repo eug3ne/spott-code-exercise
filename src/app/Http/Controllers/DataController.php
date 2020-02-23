d500b778ad913276cd78da9ace5d2d514e42bffd
eug3nius <eugene.boz@gmail.com>
2020-02-23 19:54:57 +0100

    Form Validation - Added
    Signed-off-by: Eu <eugene.boz@gmail.com>

--- src/app/Http/Controllers/DataController.php
+++ src/app/Http/Controllers/DataController.php
@@ -24,17 +24,42 @@ class DataController extends Controller
 
     public function store()
     {
-        $author = new Author();
-        $author->name = request('firstName') . ' ' . request('lastName');
-//        $author->save();
+        if ($this->formValidation()) {
+            $author = new Author();
+            $author->name = request('firstName') . ' ' . request('lastName');
+            $author->age = request('age');
+            $author->address = request('address');
+            $author->save();
 
-        $book = new Book();
-        $book->name = request('bookTitle');
-        $book->release_date = request('releaseDate');
-//        $book->save();
+            $book = new Book();
+            $book->name = request('bookTitle');
+            $book->release_date = request('releaseDate');
+        //        $book->save();
+        }
 
 //        dd($author->name);
         dd($book->name);
 
     }
+
+    /**
+     * This function is checking for the incoming inputs and validate the correct
+     * data format before saving them into the database
+     *
+     * @return boolean
+     */
+    private function formValidation(){
+        $check = request()->validate([
+            'firstName' => 'required|min 3',
+            'lastName' => 'required|min 3',
+            'age' => 'required|integer|between:1,150',
+            'address' => 'required|min 3',
+            'bookTitle' => 'required|min 3',
+            'releaseDate' => 'required|date_format:d-m-Y',
+        ]);
+
+        return $check;
+
+    }
+
 }
--- src/resources/views/main.blade.php
+++ src/resources/views/main.blade.php
@@ -23,6 +23,7 @@
     </p>
     <div class="collapse" id="collapseExample">
         <div class="card card-body">
+
             <form action="/" method="POST"  class="">
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
@@ -30,22 +31,32 @@
                     </div>
                     <input type="text" name="firstName" class="form-control" placeholder="First Name"/>
                     <input type="text" name="lastName" class="form-control" placeholder="Last Name"/>
+                    <input type="text" name="age" class="form-control" placeholder="Age"/>
+                    <input type="text" name="address" class="form-control" placeholder="Address"/>
                 </div>
 
+                <!-- Books' validation errors -->
+                @if(!empty($errors->first()))
+                    <div class="alert alert-danger" role="alert">
+                        <li>{{ $errors->first('address') }}</li>
+                        <li>{{ $errors->first('releaseDate') }}</li>
+                    </div>
+                @endif
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
                         <span class="input-group-text">Book's:</span>
                     </div>
                     <input type="text" name="bookTitle" class="form-control" placeholder="Title"/>
                     <input type="text" name="releaseDate"  class="form-control" placeholder="Release Date"/>
-
                 </div>
 
-                <button type="button" class="btn btn-outline-success">Add Book</button>
+                <button type="submit" class="btn btn-outline-success">Add Book</button>
 
                 {{--Validation for cross-origin--}}
                 @csrf
-            </form>        </div>
+            </form>
+
+        </div>
     </div>
 
     <div class="container">
